60dca1244a32c9e51395b8a6c448a1dc2ca2e701
Ryuji <ryujineer@gmail.com>
2020-01-25 04:51:29 +0900

    create component PhotoButton

---
+++ components/PhotoButton.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="photo_button__container">
+    <div class="photo_button--outside">
+      <div
+        class="photo_button--inside"
+        ontouchstart=""
+        @click="handleClick"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleClick() {
+      this.$emit('click')
+    }
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+.photo_button {
+  &__container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  &--outside {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: .2rem solid $color-white;
+    border-radius: 50%;
+    width: 5rem;
+    height: 5rem;
+  }
+
+  &--inside {
+    border: .3rem solid $color-white;
+    border-radius: 50%;
+    background-color: $color-white;
+    width: 4.2rem;
+    height: 4.2rem;
+    transition: .2s;
+
+    &:active {
+      height: 3.8rem;
+      width: 3.8rem;
+    }
+  }
+}
+</style>
---
+++ pages/taking_image.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="taking_image__container">
+    <div class="photo_button__container">
+      <photo-button
+        class="photo_button"
+        @click="takeImage"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import PhotoButton from '~/components/PhotoButton'
+
+export default {
+  layout: 'blank',
+  components: {
+    PhotoButton
+  },
+  created() {
+
+  },
+  methods: {
+    takeImage() {
+      console.log('撮影！！')
+    }
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+.taking_image__container {
+  background-color: $bg-black;
+  height: 100vh;
+}
+
+.photo_button {
+  &__container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 5rem 0;
+  }
+  position: fixed;
+  bottom:0;
+}
+</style>
