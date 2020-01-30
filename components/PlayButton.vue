c9659a4226bd9d0a7377b76f8aaabb7a64705649
Ryuji <ryujineer@gmail.com>
2020-01-30 10:39:47 +0900

    fix index style, create component PlayButtom

---
+++ components/PlayButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <div
+    class="play_button__container"
+    @click="handleClick"
+  >
+    <icon-play
+      v-if="!isPlaying"
+    />
+    <icon-pause 
+      v-if="isPlaying"
+    />
+  </div>
+</template>
+
+<script>
+import IconPlay from '~/components/icons/IconPlay'
+import IconPause from '~/components/icons/IconPause'
+
+export default {
+  components: {
+    IconPlay,
+    IconPause
+  },
+  props: {
+    isPlaying: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    handleClick() {
+      this.$emit('click')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -7,18 +7,22 @@
         :key="key"
         class="post_organism"
       >
-        <div class="organism__container">
-          <video
+        <div
+          class="post_organism__thumbnail"
+          :style="getThumbnailImage(post.thumbnail_url)"
+        >
+          <audio
+            ref="organism"
             :src="post.organism_url"
-            width="150"
-            height="150"
-            controls
           />
         </div>
         <div class="post_organism__feature">
-          <!-- <div class="post_organism__feature__play_button">
-            再生 ▶
-          </div> -->
+          <div class="post_organism__feature__play_button">
+            <play-button
+              :is-playing="isPlaying"
+              @click="playOrganism"
+            />
+          </div>
           <div class="post_organism__feature__info">
             <h3>{{ post.title }}</h3>
             <p>@{{ post.place }}</p>
@@ -40,8 +44,15 @@
 
 <script>
 import { mapState, mapActions } from 'vuex'
+import PlayButton from '~/components/PlayButton'
 
 export default {
+  components: {
+    PlayButton
+  },
+  data:() => ({
+    isPlaying: false
+  }),
   computed: {
     ...mapState({
       feedPosts: store => store.post.feedPosts
@@ -54,6 +65,19 @@ export default {
     ...mapActions('post', ['initPosts']),
     linkToRecording() {
       this.$router.push('/recording')
+    },
+    getThumbnailImage(url) {
+      return `background-image: url(${url})`
+    },
+    playOrganism() {
+      if (this.isPlaying) {
+        this.isPlaying = false
+        this.$refs.organism[0].pause()
+        return
+      }
+      this.isPlaying = true
+      console.log(this.$refs.organism)
+      this.$refs.organism[0].play()
     }
   }
 
@@ -86,7 +110,7 @@ export default {
 }
 
 .post_organism__container {
-  max-width: 60rem;
+  max-width: 80rem;
   display: flex;
   justify-content: center;
   flex-wrap: wrap;
@@ -95,24 +119,41 @@ export default {
 }
 
 .post_organism {
-  margin: 0 1.6rem 3rem;
+  margin: 0 1rem 3rem;
   border: .1rem solid $color-black;
   border-radius: 1.2rem;
 }
 
-.organism__container {
-  width: 15rem;
-  height: 15rem;
+.post_organism__thumbnail {
+  width: 16rem;
+  height: 12rem;
   border-radius: 1rem 1rem 0 0;
   overflow: hidden;
+  background-size: cover;
 }
 
 .post_organism__feature {
   display: flex;
-  justify-content: space-between;
+  width: 16rem;
+  height: 7rem;
+  background-color: $bg-black;
+  border-radius: 0 0 1rem 1rem;
+
+  &__play_button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 1rem;
+  }
+
+  &__info {
+    padding: .5rem;
+    color: $color-white;
+
+    & > p {
+      font-size: 1.2rem;
+    }
+  }
 }
 
-.post_organism__feature__info {
-  padding: .5rem;
-}
 </style>
